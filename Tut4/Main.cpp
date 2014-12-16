4215552497cb5bc3bf5d6977a87ec120e089102e
Salihan04 <salihan04@gmail.com>
2014-12-16 19:08:53 +0800

    Added colour to triangle created in OpenGLTut3

--- Tut4/Main.cpp
+++ Tut4/Main.cpp
@@ -1,7 +1,9 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <glm/glm.hpp>
 #include <iostream>
 #include "math_3d.h"
+#include "LoaderShader.h"
 
 using namespace std;
 
@@ -11,6 +13,7 @@ void createVertexBuffer();
 void initialiseGlutCallback();
 
 GLuint VBO;
+GLuint programId;
 
 int main(int argc, char **argv) {
 
@@ -18,7 +21,7 @@ int main(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(300, 300);
 	glutInitWindowSize(400, 300);
-	glutCreateWindow("Tutorial 03 - First Triangle");
+	glutCreateWindow("Tutorial 04 - Adding Colour to Triangle");
 
 	initialiseGlutCallback();
 
@@ -28,6 +31,9 @@ int main(int argc, char **argv) {
 
 		return 1;
 	}
+
+	//Need to be after glewInit(), otherwise got error
+	programId = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
 	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 
@@ -40,12 +46,16 @@ int main(int argc, char **argv) {
 
 void renderScene() {
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//use the shader
+	glUseProgram(programId);
 
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
+	//draw triangle
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glDisableVertexAttribArray(0);
@@ -54,7 +64,7 @@ void renderScene() {
 }
 
 void createVertexBuffer() {
-
+	//setting vertices of triangle
 	Vector3f vertices[3];
 	vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
 	vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
